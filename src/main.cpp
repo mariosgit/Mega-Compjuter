d86657cbd6df021fc85ee507c5c11a80bb45a912
Mario Becker <mariosge@gmail.com>
2024-10-15 00:50:39 +0200

    First try, no activity seen sofar.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <SPI.h>
 #include <U8g2lib.h>
 
+#include <mbLog.h>
+#include <elapsedMillis.h>
+
 // | D53 | CS |
 // | D49 | DC |
 // | D51(mosi) | DI |
@@ -10,11 +13,10 @@
 
 const uint8_t pinCS = 53;
 const uint8_t pinDC = 49;
-const uint8_t pinRST = -1;
 const uint8_t pinMOSI = 51;
 const uint8_t pinSCLK = 52;
 
-U8G2_SSD1306_128X64_NONAME_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ pinCS, /* dc=*/ pinDC);
+U8G2_SH1106_128X64_NONAME_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ pinCS, /* dc=*/ pinDC);
 
 
 // UB8833 connections
@@ -25,11 +27,21 @@ U8G2_SSD1306_128X64_NONAME_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ pinCS, /* dc=*/ pi
 #define ADDR_H_DIR DDRA
 #define ADDR_L_DIR DDRC // same as data, must be latched with AS
 
+#define DATA_OUT (PORTC)
+#define DATA_IN  (PINC)
+#define ADDR_H   (PINC)
+#define ADDR_L   (PINA)
+
+// PORTA - UB8833 High Adr
+// PORTC - UB8833 Low Adr / Data
+
 const uint8_t pinAS = 2;
 const uint8_t pinDS = 3;
 const uint8_t pinRST = 45;
 const uint8_t pinXTAL = 46;
 const uint8_t pinRW = 47;
+
+elapsedMillis emDisplay = 0;
 
 void setup()
 {
@@ -46,27 +58,74 @@ void setup()
     pinMode(pinRST, OUTPUT);
     pinMode(pinRW, INPUT);
     pinMode(pinXTAL, OUTPUT);
-    digitalWrite(pinRST, LOW);
-    analogWrite(pinXTAL, 128); // clock with 50%
-
-
     digitalWrite(pinRST, HIGH);
+    // analogWrite(pinXTAL, 128); // clock with 50%
+
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
+    if(emDisplay > 1000)
+    {
+        emDisplay = 0;
 
-    u8g2.clearDisplay();
-    u8g2.setCursor(7,7);
-    u8g2.setDrawColor(1);
-    u8g2.drawStr(0,20,"Hello World!");
-    u8g2.drawStrX2(0,40,"JU+TE");
-    u8g2.drawLine(0,0, 127, 63);
-    u8g2.updateDisplay();
+        LOG <<"---------------------" <<LOG.endl;
+        LOG <<"Hello Mega Comp Ju+Te r" <<LOG.endl;
 
-    Serial.println("Hello Mega Comp Ju+Te r");
-    delay(1000);
+        int as = 0;
+
+        // start 8833 and watch
+        digitalWrite(pinRST, HIGH);
+
+        uint8_t dataa = 0;
+        uint8_t datac = 0;
+
+        for(int i = 0; i < 5000; i++)
+        {
+            digitalWrite(pinXTAL, LOW);
+            digitalWrite(pinXTAL, HIGH);
+
+            digitalWrite(pinXTAL, LOW);
+            digitalWrite(pinXTAL, HIGH);
+
+            digitalWrite(pinXTAL, LOW);
+            digitalWrite(pinXTAL, HIGH);
+
+            digitalWrite(pinXTAL, LOW);
+            digitalWrite(pinXTAL, HIGH);
+
+            digitalWrite(pinXTAL, LOW);
+            digitalWrite(pinXTAL, HIGH);
+
+            digitalWrite(pinXTAL, LOW);
+            digitalWrite(pinXTAL, HIGH);
+
+            digitalWrite(pinXTAL, LOW);
+            digitalWrite(pinXTAL, HIGH);
+
+            digitalWrite(pinXTAL, LOW);
+            digitalWrite(pinXTAL, HIGH);
+
+            dataa |= PINA;
+            datac |= PINC;
+            // as = digitalRead(pinAS);
+            // LOG <<"as" <<as <<" d/low:" <<PINC <<" hi:" <<PINA <<LOG.endl;
+        }
+
+        digitalWrite(pinRST, LOW);
+
+        char sbuf[20];
+        u8g2.clearDisplay();
+        u8g2.setCursor(7,7);
+        u8g2.setDrawColor(1);
+        u8g2.drawStrX2(0,16,"JU+TE");
+
+        snprintf(sbuf, 20, "A:%x D:%x", dataa, datac);
+        u8g2.drawStr(0,30, sbuf);
+        // u8g2.drawLine(0,0, 127, 63);
+        u8g2.updateDisplay();
+
+    }
 }
 
 // put function definitions here:
